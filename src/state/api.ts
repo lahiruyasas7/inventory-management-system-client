e8e499b9a1f1611cffaece5f491c6af44b066789
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-12-29 18:49:46 +0530

    feat(api): implement dashboard metrics query using RTK Query

--- src/state/api.ts
+++ src/state/api.ts
@@ -1,11 +1,16 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
-import reducer from ".";
+import { DashboardMetrics } from "@/app/types/types";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
-    reducerPath: 'api',
-    tagTypes: [],
-    endpoints: (build) => ({}),
-})
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+  reducerPath: "api",
+  tagTypes: ["DashboardMetrics"],
+  endpoints: (build) => ({
+    getDashboardMetrics: build.query<DashboardMetrics, void>({
+      query: () => "/dashboard",
+      providesTags: ["DashboardMetrics"]
+    }),
+  }),
+});
 
-export const {} = api;
+export const {useGetDashboardMetricsQuery} = api;
